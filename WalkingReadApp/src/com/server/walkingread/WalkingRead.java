ecf59b5232e6cb7248d0dbcc631ca4e412abbb2d
WalkingRead <the.walking.read@gmail.com>
2013-06-01 18:15:31 +0200

    Añadida la web, las pestanyas, el prototipo de lista y el apartado de incidencias

--- WalkingReadApp/src/com/server/walkingread/WalkingRead.java
+++ WalkingReadApp/src/com/server/walkingread/WalkingRead.java
@@ -1,11 +1,14 @@
 package com.server.walkingread;
 
 import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.TabHost;
 
 public class WalkingRead extends TabActivity {
+	// Pruebas|Temporal
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,14 +17,20 @@ public class WalkingRead extends TabActivity {
 
 		TabHost tabHost = getTabHost();
 		tabHost.addTab(tabHost.newTabSpec("tab1")
-				.setIndicator("Noticias", null).setContent(R.id.tab1));
+				.setIndicator("Noticias", null)
+				.setContent(new Intent(getApplicationContext(), News.class)));
 		tabHost.addTab(tabHost.newTabSpec("tab2")
-				.setIndicator("Usuarios", null).setContent(R.id.tab2));
+				.setIndicator("Usuarios", null)
+				.setContent(new Intent(getApplicationContext(), Users.class)));
+		tabHost.addTab(tabHost.newTabSpec("tab2")
+				.setIndicator("Incidencias", null)
+				.setContent(new Intent(getApplicationContext(), Reports.class)));
 
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
+		//MENU
 		getMenuInflater().inflate(R.menu.walking_read, menu);
 		return true;
 	}
